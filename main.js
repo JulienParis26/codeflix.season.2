62059a95056cb00c18c82c241704e7c5c93f4503
JulienParis26 <71380678+JulienParis26@users.noreply.github.com>
2020-11-02 15:44:13 +0100

    Adding main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const { empty, withArgs } = require('./eventbox')
+const { duplicate, transform } = require('./streambox')
+
+
+    console.log("---- Eventbox ----")
 
 empty()
 withArgs([
@@ -10,3 +14,12 @@ withArgs([
     'Sanji',
     'Ch0pper'
 ])
+
+    console.log("---- Streambox ----")
+
+duplicate('main.js')
+transform(
+    'eventbox.js', // filename
+    /[a-z]/g,      // re ( RegExp -> Regular expression )
+    (letter) => letter.toUpperCase(), // fn -> function
+)
